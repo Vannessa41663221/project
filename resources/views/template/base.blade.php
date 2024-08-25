5675e73b85bcea154c0e7e8cbc27117af55ed6bf
H-A-L-F <62553950+Vannessa41663221@users.noreply.github.com>
2024-08-25 10:40:07 +0700

    added tailwind cdn

--- resources/views/template/base.blade.php
+++ resources/views/template/base.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.2/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
 <div class="w-screen h-screen">
